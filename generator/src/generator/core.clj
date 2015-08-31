a86ba4a83fa26d4f8ecdee8413de6b748df6dbbc
hackenfreude <hackenfreude@users.noreply.github.com>
2015-08-30 17:32:56 -0700

    strip generator down to bare bones

--- generator/src/generator/core.clj
+++ generator/src/generator/core.clj
@@ -2,20 +2,16 @@
   (:gen-class)
   (:require [langohr.core :as rmq]
             [langohr.channel :as lch]
-            [langohr.queue :as lq]
-            [langohr.consumers :as lc]
             [langohr.basic :as lb]))
-
-(def ^{:const true}
-  default-exchange-name "logs")
 
 (defn -main
   [& args]
   (let [conn (rmq/connect {:host "rabbit"})
         ch (lch/open conn)
-        qname "langohr.examples.hello-world"]
+        routing-key ""
+        exchange "logs"]
     (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
-    (lb/publish ch default-exchange-name qname "Hello!" {:content-type "text/plain" :type "greetings.hi"})
+    (lb/publish ch exchange routing-key "Hello!")
     (Thread/sleep 2000)
     (println "[main] Disconnecting...")
     (rmq/close ch)
