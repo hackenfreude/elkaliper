70586c0e82af459bf25699820838fa0e6f1b993f
hackenfreude <hackenfreude@users.noreply.github.com>
2015-12-14 20:13:56 -0800

    now with json

--- generator/project.clj
+++ generator/project.clj
@@ -2,7 +2,8 @@
   :description "create traffic to rabbit for toy project"
   :license {:name "The MIT LIcense"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.novemberain/langohr "3.3.0"]]
+                 [com.novemberain/langohr "3.3.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :main generator.core
   :profiles {:dev {:dependencies [[midje "1.7.0"]]}}
   :plugins [[lein-midje "3.1.3"]]
--- generator/src/generator/core.clj
+++ generator/src/generator/core.clj
@@ -2,7 +2,8 @@
   (:gen-class)
   (:require [langohr.core :as rmq]
             [langohr.channel :as lch]
-            [langohr.basic :as lb]))
+            [langohr.basic :as lb]
+            [clojure.data.json :as json]))
 
 (defn -main
   [& args]
@@ -12,7 +13,7 @@
         exchange "logs"]
     (Thread/sleep 10000)
     (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
-    (dotimes [n 1000] (lb/publish ch exchange routing-key "Hello!"))
+    (dotimes [n 1000] (lb/publish ch exchange routing-key (json/write-str {:a 1 :b {:foo "bar"}})))
     (Thread/sleep 2000)
     (println "[main] Disconnecting...")
     (rmq/close ch)
